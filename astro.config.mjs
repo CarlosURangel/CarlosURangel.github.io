72f5b2b55fbc61e17a3b72e0ec373f1cd6d52b0b
CarlosURangel <crangel30@alumnos.uaq.mx>
2024-05-30 00:25:59 -0600

    branch dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CarlosURangel.github.io',
-  base: '/Wallon',
+
   integrations: [tailwind()],
   buildOptions: {
     // Esta opción desactiva la visualización del estado de desarrollo
