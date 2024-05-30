c4b310aad095d1d911529a1f4e5f63d33b6254f3
CarlosURangel <crangel30@alumnos.uaq.mx>
2024-05-30 00:05:48 -0600

    movi algo del archivo deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://CarlosURangel.github.io',
+  base: '/Wallon',
   integrations: [tailwind()],
   buildOptions: {
     // Esta opción desactiva la visualización del estado de desarrollo
